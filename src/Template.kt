16c027f9be04ad80429edc3fe310065d7aefac3a
Chris Bradford <chjabr@gmail.com>
2023-12-08 23:38:02 -0600

    Update template

--- src/Template.kt
+++ src/Template.kt
@@ -1,15 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var answer = 0
+        for (line in input) {
+
+        }
+        return answer
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
-    }
+        var answer = 0
+        for (line in input) {
 
-    // test if implementation meets criteria from the description, like:
-//    val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 1)
+        }
+        return answer
+    }
 
     val input = readInput("Day01")
     part1(input).println()
