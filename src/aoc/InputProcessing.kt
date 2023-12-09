45b73be3907cb9e9f38f18c2ef3bef3c94de3451
Chris Bradford <chjabr@gmail.com>
2023-12-09 13:19:24 -0600

    Add function for parsing words out of strings

--- src/aoc/InputProcessing.kt
+++ src/aoc/InputProcessing.kt
@@ -15,3 +15,9 @@ fun String.parseDoubles(): List<Double> {
     val regex = Regex("-?\\d+\\.?\\d*")
     return regex.findAll(this).map { it.value.toDouble() }.toList()
 }
+
+fun String.parseWords(includeNums: Boolean = false): List<String> {
+    val pattern = if (includeNums) "\\w+" else "[a-zA-Z]+"
+    val regex = Regex(pattern)
+    return regex.findAll(this).map { it.value }.toList()
+}
