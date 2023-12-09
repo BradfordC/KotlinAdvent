ed230da619679ee8347bd98c973e7d4a324dc633
Chris Bradford <chjabr@gmail.com>
2023-12-08 19:20:56 -0600

    Day 08 Part 2

--- src/Day08.kt
+++ src/Day08.kt
@@ -1,7 +1,19 @@
-fun main() {
-    fun part1(input: List<String>): Int {
-        var answer = 0
+import aoc.*
 
+fun main() {
+
+    fun timeToZ(start: String, map: Map<String, List<String>>, inputs: List<Int>): Int {
+        var answer = 0
+        var loc = start
+        // Only 1 XXZ entry per cycle
+        while (!loc.endsWith("Z")) {
+            loc = map[loc]!![inputs[answer % inputs.size]]
+            answer++
+        }
+        return answer
+    }
+
+    fun part1(input: List<String>): Int {
         val inputs = input[0].toList().map { if (it == 'L') 0 else 1 }.toList()
         val map = mutableMapOf<String, List<String>>()
         for (line in input.subList(2, input.size)) {
@@ -11,13 +23,7 @@ fun main() {
             map[key] = listOf(left, right)
         }
 
-        var loc = "AAA"
-        while (loc != "ZZZ") {
-            loc = map[loc]!![inputs.get(answer % inputs.size)]
-            answer++
-        }
-
-        return answer
+        return timeToZ("AAA", map, inputs)
     }
 
     fun part2(input: List<String>): Long {
@@ -32,18 +38,17 @@ fun main() {
             map[key] = listOf(left, right)
         }
 
-        val locs = map.keys.filter { it.endsWith("A") }.toMutableList()
-        var index = 0
-        while (locs.find { !it.endsWith("Z") } != null) {
-            for (i in locs.indices) {
-                locs[i] = map[locs[i]]!![inputs.get(index)]
-            }
-            index++
-            index %= inputs.size
-            answer++
-        }
-
-        return answer
+        val starts = map.keys.filter { it.endsWith("A") }
+        /**
+         * Found through experimentation that:
+         * 1. Each start point leads to an independent cycle
+         * 2. Each cycle only contains one XXZ entry
+         * 3. The time it takes to reach XXZ the first time is equal to the cycle length
+         * Those three factors mean it's OK to take the least common multiple of timeToZ() for each input,
+         * even though that wouldn't work for a general solution
+          */
+        val times = starts.map { timeToZ(it, map, inputs).toLong() }
+        return lcm(times)
     }
 
     // test if implementation meets criteria from the description, like:
@@ -52,5 +57,5 @@ fun main() {
 
     val input = readInput("Day08")
     part1(input).println()
-//    part2(input).println()
+    part2(input).println()
 }
---
+++ src/aoc/Math.kt
@@ -0,0 +1,72 @@
+package aoc
+
+import kotlin.math.*
+
+fun Long.isPrime(): Boolean {
+    if (this == 2L) {
+        return true
+    }
+    if (this % 2 == 0L) {
+        return false
+    }
+    val maxCheck = sqrt(this.toDouble()).toInt()
+    for (i in 3..maxCheck step 2) {
+        if (this % i != 0L) {
+            return false
+        }
+    }
+    return true
+}
+
+fun Int.isPrime(): Boolean {
+    return this.toLong().isPrime()
+}
+
+fun Long.primeFactors(): List<Long> {
+    val primes = mutableListOf<Long>()
+    var remaining = this
+    while(remaining != 1L) {
+        val maxCheck = sqrt(remaining.toDouble()).toLong()
+        var foundFactor = false
+        for (i in 2..maxCheck) {
+            if (remaining % i == 0L) {
+                primes.add(i)
+                remaining /= i
+                foundFactor = true
+                break
+            }
+        }
+        if (!foundFactor) {
+            primes.add(remaining)
+            remaining = 1
+        }
+    }
+    return primes
+}
+
+fun Int.primeFactors(): List<Int> {
+    return this.toLong().primeFactors().map { it.toInt() }.toList()
+}
+
+fun lcm(vararg values: Long): Long {
+    return lcm(values.toList())
+}
+
+fun lcm(values: Iterable<Long>): Long {
+    val factorCounts = values.map { it.primeFactors().groupingBy { it }.eachCount() }
+    val combined = mutableMapOf<Long, Int>()
+    for (factors in factorCounts) {
+        for ((k, v) in factors) {
+            combined[k] = max((combined[k] ?: 0), v)
+        }
+    }
+    return combined.entries.fold(1L) { acc, v -> acc * v.key.toDouble().pow(v.value).toLong() }
+}
+
+fun lcm(vararg values: Int): Int {
+    return lcm(values.map { it.toLong() }).toInt()
+}
+
+fun lcm(values: Iterable<Int>): Int {
+    return lcm(values.map { it.toLong() }).toInt()
+}
